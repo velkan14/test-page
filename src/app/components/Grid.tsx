91a4ec63dc640fad3bf8771e5a887c6f4cf955d3
Daniel Amado <velkan14@gmail.com>
2024-01-19 18:35:46 +0100

    Fix type error Grid

--- src/app/components/Grid.tsx
+++ src/app/components/Grid.tsx
@@ -1,4 +1,10 @@
-export default function Grid({ children }) {
+import { ReactNode } from "react";
+
+interface Props {
+  children?: ReactNode;
+}
+
+export default function Grid({ children }: Props) {
   return (
     <div className="mb-32 grid text-center lg:max-w-6xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
       {children}
